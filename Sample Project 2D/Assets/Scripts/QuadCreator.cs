708b601f9444f44f36f71b87a3ca715beb0fba17
Jarut-Kulthawaiphorn <jarut@progaming.co.th>
2023-01-05 17:35:48 +0700

    Create a quad from mouse input

--- Sample Project 2D/Assets/Scripts/QuadCreator.cs
+++ Sample Project 2D/Assets/Scripts/QuadCreator.cs
@@ -1,57 +1,76 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class QuadCreator : MonoBehaviour
 {
-    public float width = 1;
-    public float height = 1;
+    Vector3[] vertices = new Vector3[4];
+    public int verticeIndex = 0;
+    MeshFilter meshFilter;
+    Mesh mesh;
 
     public void Start()
     {
         MeshRenderer meshRenderer = gameObject.AddComponent<MeshRenderer>();
         meshRenderer.sharedMaterial = new Material(Shader.Find("Standard"));
+        mesh = new Mesh();
+        meshFilter = gameObject.AddComponent<MeshFilter>();
+    }
 
-        MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
-
-        Mesh mesh = new Mesh();
-
-        Vector3[] vertices = new Vector3[4]
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
         {
-            new Vector3(0, 0, 0),
-            new Vector3(width, 0, 0),
-            new Vector3(0, height, 0),
-            new Vector3(width, height, 0)
-        };
-        mesh.vertices = vertices;
+            Vector3 mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            mousePos = new Vector3(mousePos.x, mousePos.y, mousePos.z + 1);
 
-        int[] tris = new int[6]
-        {
-            // lower left triangle
-            0, 2, 1,
-            // upper right triangle
-            2, 3, 1
-        };
-        mesh.triangles = tris;
+            Debug.Log(mousePos);
+            if(verticeIndex <= 3 )
+            {
+                vertices[verticeIndex] = mousePos;
+                verticeIndex++;
+            }
+            else
+            {
+                verticeIndex = 0;
+            }
 
-        Vector3[] normals = new Vector3[4]
-        {
-            -Vector3.forward,
-            -Vector3.forward,
-            -Vector3.forward,
-            -Vector3.forward
-        };
-        mesh.normals = normals;
+            if (verticeIndex == 4)
+            {
+                mesh.vertices = vertices;
+                int[] tris = new int[6]
+                {
+                    // lower left triangle
+                    0, 2, 1,
+                    // upper right triangle
+                    2, 3, 1
+                };
+                mesh.triangles = tris;
 
-        Vector2[] uv = new Vector2[4]
-        {
-            new Vector2(0, 0),
-            new Vector2(1, 0),
-            new Vector2(0, 1),
-            new Vector2(1, 1)
-        };
-        mesh.uv = uv;
+                Vector3[] normals = new Vector3[4]
+                {
+                    -Vector3.forward,
+                    -Vector3.forward,
+                    -Vector3.forward,
+                    -Vector3.forward
+                };
+                mesh.normals = normals;
 
-        meshFilter.mesh = mesh;
+                Vector2[] uv = new Vector2[4]
+                {
+                    new Vector2(0, 0),
+                    new Vector2(1, 0),
+                    new Vector2(0, 1),
+                    new Vector2(1, 1)
+                };
+                mesh.uv = uv;
+
+                meshFilter.mesh = mesh;
+            }
+        }
+
+        
     }
 }
