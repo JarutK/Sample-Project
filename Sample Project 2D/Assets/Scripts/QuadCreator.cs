95eef5cd21b429f9e9967da3048ba613275db475
Jarut-Kulthawaiphorn <jarut@progaming.co.th>
2023-01-06 17:23:34 +0700

    First Demo

--- Sample Project 2D/Assets/Scripts/QuadCreator.cs
+++ Sample Project 2D/Assets/Scripts/QuadCreator.cs
@@ -7,70 +7,133 @@ public class QuadCreator : MonoBehaviour
 {
     Vector3[] vertices = new Vector3[4];
     public int verticeIndex = 0;
-    MeshFilter meshFilter;
+    [SerializeField] Material imageMaterial;
+    [SerializeField] GameObject quadPref;
     Mesh mesh;
 
     public void Start()
     {
-        MeshRenderer meshRenderer = gameObject.AddComponent<MeshRenderer>();
-        meshRenderer.sharedMaterial = new Material(Shader.Find("Standard"));
-        mesh = new Mesh();
-        meshFilter = gameObject.AddComponent<MeshFilter>();
+        quadPref.GetComponent<MeshRenderer>().sharedMaterial = imageMaterial;
     }
 
     private void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        
+    }
+
+    void GenerateQuad()
+    {
+        mesh = new Mesh();
+        mesh.vertices = vertices;
+        int[] tris = new int[6]
+        {
+            // lower left triangle
+            0, 2, 1,
+            // upper right triangle
+            2, 3, 1
+        };
+        mesh.triangles = tris;
+
+        Vector3[] normals = new Vector3[4]
+        {
+            -Vector3.forward,
+            -Vector3.forward,
+            -Vector3.forward,
+            -Vector3.forward
+        };
+        mesh.normals = normals;
+
+        mesh.uv = UV();
+
+        quadPref.GetComponent<MeshFilter>().mesh = mesh;
+        quadPref.GetComponent<MeshCollider>().sharedMesh = mesh;
+
+        Instantiate(quadPref);
+    }
+
+    Vector2 MidPosition()
+    {
+        float totalX = 0f;
+        float totalY = 0f;
+        for(int i = 0; i < vertices.Length ; i++)
+        {
+            totalX += vertices[i].x;
+            totalY += vertices[i].y;
+        }
+
+        Vector2 center = new Vector2(totalX / vertices.Length, totalY / vertices.Length);
+        return center;
+    }
+
+    void OrderPosition()
+    {
+        Vector2 center = MidPosition();
+        Vector2[] tempPos = new Vector2[vertices.Length];
+        for(int i = 0; i < 4 ; i++)
+        {
+            //vertice 0
+            if(vertices[i].x <= center.x && vertices[i].y <= center.y)
+            {
+                tempPos[0] = vertices[i];
+            }
+            //vertice 1
+            if(vertices[i].x >= center.x && vertices[i].y <= center.y)
+            {
+                tempPos[1] = vertices[i];
+            }
+            //vertice 2
+            if(vertices[i].x <= center.x && vertices[i].y >= center.y)
+            {
+                tempPos[2] = vertices[i];
+            }
+            //vertice 3
+            if(vertices[i].x >= center.x && vertices[i].y >= center.y)
+            {
+                tempPos[3] = vertices[i];
+            }
+        }
+        for(int j = 0; j < vertices.Length; j++)
+        {
+            vertices[j] = tempPos[j];
+        }
+    }
+
+    Vector2[] UV()
+    {
+        Vector2[] uv = new Vector2[4];
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            uv[i] = new Vector3((vertices[i].x + 8) / 16, (vertices[i].y + 4) / 8, vertices[i].z);
+        }
+
+        return uv;
+    }
+
+    private void OnMouseDown()
+    {
+
+        Vector3 scale = transform.localScale;
+
+        if (Input.GetMouseButtonDown(0))
         {
             Vector3 mousePos = Input.mousePosition;
             mousePos = Camera.main.ScreenToWorldPoint(mousePos);
             mousePos = new Vector3(mousePos.x, mousePos.y, mousePos.z + 1);
 
-            Debug.Log(mousePos);
-            if(verticeIndex <= 3 )
+            if (verticeIndex <= vertices.Length - 1)
             {
                 vertices[verticeIndex] = mousePos;
                 verticeIndex++;
-            }
-            else
-            {
-                verticeIndex = 0;
-            }
-
-            if (verticeIndex == 4)
-            {
-                mesh.vertices = vertices;
-                int[] tris = new int[6]
+                if (verticeIndex == vertices.Length)
                 {
-                    // lower left triangle
-                    0, 2, 1,
-                    // upper right triangle
-                    2, 3, 1
-                };
-                mesh.triangles = tris;
-
-                Vector3[] normals = new Vector3[4]
-                {
-                    -Vector3.forward,
-                    -Vector3.forward,
-                    -Vector3.forward,
-                    -Vector3.forward
-                };
-                mesh.normals = normals;
-
-                Vector2[] uv = new Vector2[4]
-                {
-                    new Vector2(0, 0),
-                    new Vector2(1, 0),
-                    new Vector2(0, 1),
-                    new Vector2(1, 1)
-                };
-                mesh.uv = uv;
-
-                meshFilter.mesh = mesh;
+                    verticeIndex = 0;
+                    OrderPosition();
+                    GenerateQuad();
+                    
+                }
             }
         }
 
-        
     }
 }
