e5f5c64f1dcf8781959cb685f5acc248bd365112
Jarut-Kulthawaiphorn <jarut@progaming.co.th>
2023-01-06 17:54:34 +0700

    Create Player

--- Sample Project 2D/Assets/Scripts/PlayerController.cs
+++ Sample Project 2D/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] float Speed;
+    float horizontal;
+    float vertical;
+    Rigidbody rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+        rigidbody.velocity = new Vector3(horizontal * Speed, vertical * Speed, 0);
     }
 }
